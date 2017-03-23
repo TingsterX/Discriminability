022daebb41640ea9bfce1ec8f08d1616f415bf12
ebridge2 <ebridge2@jhu.edu>
2017-03-22 20:44:08 -0400

    updated documentation to be more specific, and added ability to plot correlation matrix with only a timeseries

--- R/plot_square.R
+++ R/plot_square.R
@@ -2,15 +2,19 @@
 #'
 #' A function that plots a square matrix. Good for things like Correlation matrices.
 #'
-#' @param signal [[n]][nt, nroi]: the signal for each of the n subjects, containing an array of nt observations for nroi rois.
+#' @param in [nroi, nroi] or [nt, nroi]: the input. Can either be a square matrix of featuresxfeatures, or an array of observationsxfeatures (see itype parameter).
 #' @param title="": the title for the square plot.
 #' @param xlabel="ROIs": the x label for the square plot.
 #' @param ylabel="ROIs": the y label for the square plot.
 #' @param legend="": the legend title for the square plot.
+#' @param itype="sq": the shape of the input. If "sq", the plot will be generated as is for the input signal. If "ts", we will assume the input is observationsxfeatures, and will correlate the features first.
 #' @return sqplot : a plot of the square.
 #' @author Eric Bridgeford
 #' @export
-plot_square <- function(mtx, title="",xlabel="ROI", ylabel="ROI", legend="metric") {
+plot_square <- function(mtx, title="",xlabel="ROI", ylabel="ROI", legend="metric", itype="sq") {
+  if (itype == "ts") {
+    mtx <- cor(mtx)  # if a timeseries is passed in, correlate the features first
+  }
   require(reshape2)
   require(ggplot2)
   sqplot <- ggplot(melt(mtx), aes(x=Var1, y=Var2, fill=value)) +
--- R/plot_timeseries.R
+++ R/plot_timeseries.R
@@ -2,7 +2,7 @@
 #'
 #' A function that plots a timeseries.
 #'
-#' @param signal [[n]][nt, nroi]: the signal for each of the n subjects, containing an array of nt observations for nroi rois.
+#' @param ts[nt, nroi]: the signal, containing an array of nt observations for nroi rois.
 #' @param title="": the title for the timeseries plot.
 #' @param xlabel="TRs": the x label for the timeseries plot.
 #' @param ylabel="intensity": the y label for the timeseries plot.
@@ -17,7 +17,7 @@ plot_timeseries <- function(ts, title="",xlabel="TRs", ylabel="intensity", legen
   colnames(tsm) <- c(xlabel, legend, ylabel)
   tsplot <- ggplot(data=tsm, aes_string(x=xlabel, y=ylabel, group=legend, color=legend)) +
     geom_line(alpha=.2) +
-    scale_y_discrete() +
+    scale_y_continuous() +
     ylab(ylabel) +
     xlab(xlabel) +
     ggtitle(title)
